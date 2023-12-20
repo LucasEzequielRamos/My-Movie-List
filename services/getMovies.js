19a4451679351e0ba6bc3084586a264778f65ea6
Lucas Becerra <lucaseramos13@gmail.com>
2023-12-20 20:46:04 -0300

    implement api search

--- services/getMovies.js
+++ services/getMovies.js
@@ -1,7 +1,13 @@
-import mockMovie from '../mock/mockMovie.json'
-
-function getMovieData () {
-  return mockMovie
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZGRkN2I0NjhiMDFkZDcyOWNmY2YxY2JjOTRjZTdmZCIsInN1YiI6IjY0OTllODkzYmJkMGIwMDEwNjZmZGJkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aVRF0b2kv5eizCeIAK3VE4Lr-6LAVocMMHrWTDU9gfk'
+  }
 }
 
-export const movieData = getMovieData()
+export async function getMovieData (query) {
+  const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
+  const data = await res.json()
+  return data
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
+import { useState } from 'react'
+import { getMovieData } from '../services/getMovies'
+
 const App = () => {
+  const [query, setQuery] = useState('')
+  const [data, setData] = useState([])
+
+  const handleChange = (event) => {
+    const value = event.target.value
+    setQuery(value)
+    getResults(value)
+  }
+
+  async function getResults (query) {
+    const res = await getMovieData(query)
+    setData(res)
+  }
+
   return (
     <>
       <header className="flex flex-col justify-center items-center m-10">
         <h1 className="text-6xl m-5">The movie list</h1>
         <nav className="flex gap-5">
           <form >
-            <input type="text" className="border border-black" />
+            <input onChange={handleChange} value={query} type="text" className="border border-black" />
           </form>
         </nav>
       </header>
       <main>
-        <section className="flex w-4/5 m-auto bg-red-200">
-          <div className="flex flex-col bg-slate-700 p-5 rounded-xl">
-            <img className="w-40" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaXUTkGriCUtWlEEaOVIfO69qrUUp5vH7uQ_DMvgtIvA&s" alt="" />
-            <h2 className="text-xl font-bold text-white">Avengers</h2>
-            <p className="bg-red-600 w-10 text-center p-1 rounded-full text-xs font-bold" >210</p>
-          </div>
+        <section className="flex flex-wrap w-4/5 m-auto bg-red-200 gap-5 p-5 justify-center">
+          {data.results?.map((movie) => (
+            <div key={movie.id} className="flex flex-col  w-[200px]  bg-slate-700 p-5 rounded-xl">
+              <img className="w-40" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+              <h2 className="text-xl font-bold text-white">{movie.title}</h2>
+              <p className="bg-red-600 w-14 text-center p-1 rounded-full text-xs font-bold" >{movie.popularity}</p>
+            </div>
+          )
+          )}
         </section>
       </main>
     </>
